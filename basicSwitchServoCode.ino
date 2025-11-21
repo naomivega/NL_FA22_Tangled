55f5b172df61ace1e2153f845932467606bb1718
elizabethjhan04 <elizabethjhan04@gmail.com>
2025-11-21 16:19:59 -0600

    Update basicSwitchServoCode.ino

--- basicSwitchServoCode.ino
+++ basicSwitchServoCode.ino
@@ -1,20 +1,18 @@
 #include <Servo.h>
 
-// Copper tape trigger pins
-const int scene1Pin = 2;
-const int scene2Pin = 3;
-const int scene3Pin = 4;
-const int scene4Pin = 5;
-const int scene5Pin = 6;
+const int scene1Pin = A0;
+const int scene2Pin = A1;
+const int scene3Pin = A2;
+const int scene4Pin = A3;
+const int scene5Pin = A4;  // LED trigger
 
-// Servos
-Servo servo1; 
-Servo servo2; 
-Servo servo3; 
-Servo servo4; 
-Servo servo5; 
+const int ledPin = 9;      // controls all 3 LEDs
 
-// Scene completion flags
+Servo servo1;  // scene 1
+Servo servo2;  // scene 2
+Servo servo3;  // scene 3
+Servo servo4;  // scene 4 360 servo
+
 bool scene1Done = false;
 bool scene2Done = false;
 bool scene3Done = false;
@@ -22,87 +20,76 @@ bool scene4Done = false;
 bool scene5Done = false;
 
 void setup() {
-  // Input pins
+
   pinMode(scene1Pin, INPUT);
   pinMode(scene2Pin, INPUT);
   pinMode(scene3Pin, INPUT);
   pinMode(scene4Pin, INPUT);
   pinMode(scene5Pin, INPUT);
 
-  // Attach servos
-  servo1.attach(10);  servo1.write(0);
-  servo2.attach(11);  servo2.write(90);
-  servo3.attach(12);  servo3.write(0);
-  servo4.attach(13);  servo4.write(40);
-  servo5.attach(14);  servo5.write(180);
+  // servos
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(10);
+
+  // LEDs
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
 
-  // scene 1
+  // scene1: snappy movement for banners
   if (!scene1Done && digitalRead(scene1Pin) == HIGH) {
     for (int pos = 0; pos <= 90; pos++) {
       servo1.write(pos);
-      delay(15);
+      delay(5); //speed
     }
     scene1Done = true;
   }
 
-  // scene 2
+  // scene2: girls move back and forth
   if (!scene2Done && digitalRead(scene2Pin) == HIGH) {
-
-    // Back and forth sweep for 15 seconds
-    unsigned long start = millis();
-    while (millis() - start < 15000) {
-      for (int pos = 10; pos <= 170; pos++) {
+    for (int pos = 40; pos <= 140; pos++) {
+      servo2.write(pos);
+      delay(10);
+      if (digitalRead(scene2Pin) == LOW) break; 
+    }
+    if (digitalRead(scene2Pin) == HIGH) {
+      for (int pos = 140; pos >= 40; pos --) {
         servo2.write(pos);
         delay(10);
-      }
-      for (int pos = 170; pos >= 10; pos--) {
-        servo2.write(pos);
-        delay(10);
+        if (digitalRead(scene2Pin) == LOW) break;
       }
     }
-
+  } else if (!scene2Done && digitalRead(scene2Pin) ==LOW) {
+    servo2.write(90);
     scene2Done = true;
   }
 
-  // scene 3
+  // scene3: guards slowly move towards rapunzel and flynn
   if (!scene3Done && digitalRead(scene3Pin) == HIGH) {
-
-    for (int pos = 0; pos <= 90; pos++) {
+    for (int pos =0; pos <=90; pos++) {
       servo3.write(pos);
-      delay(20);
+      delay(30);
     }
-
     scene3Done = true;
   }
 
-  // scene 4
-  if (!scene4Done && digitalRead(scene4Pin) == HIGH) {
-
-    int leftPos  = 40;
-    int rightPos = 140;
-
-    for (int i = 0; i < 12; i++) {
-      servo4.write(leftPos);
-      delay(120);
-      servo4.write(rightPos);
-      delay(120);
+  // scene4: crowd continuously rotates, dancing
+  if (!scene4Done) {
+    if (digitalRead(scene4Pin) == HIGH) {
+      servo4.write(180);
+    } else if (scene4Done == false) {
+      servo4.write(90);
+      scene4Done = true;
     }
-
-    servo4.write(90);
-    scene4Done = true;
   }
 
-  // scene 5
+  // scene5; lanterns turn on
   if (!scene5Done && digitalRead(scene5Pin) == HIGH) {
-
-    for (int pos = 180; pos >= 0; pos--) {
-      servo5.write(pos);
-      delay(40);
-    }
-
-    scene5Done = true;
+    digitalWrite(ledPin, HIGH);   // turns on all LEDs
+    scene5Done = true;            // stay on forever
   }
 }
